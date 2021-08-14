3aaa59cc2e615cb7faf7a9be9a550b5933773f55
voidniloy <niloy@sawolabs.com>
2021-08-14 12:30:52 +0530

    Updated .env

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template,request
-from sawo import createTemplate,verifyToken
+from flask import Flask, render_template, request
+from sawo import createTemplate, verifyToken
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 
 app = Flask(__name__)
-createTemplate("templates/partials",flask=True) #using flask = True genrates flask template
+# using flask = True genrates flask template
+createTemplate("templates/partials", flask=True)
 
 load = ''
 loaded = 0
@@ -13,36 +20,40 @@ def setPayload(payload):
     global load
     load = payload
 
+
 def setLoaded(reset=False):
     global loaded
     if reset:
-        loaded=0
+        loaded = 0
     else:
-        loaded+=1
+        loaded += 1
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/login_page")
 def login_page():
     setLoaded()
-    setPayload(load if loaded<2 else '')
+    setPayload(load if loaded < 2 else '')
     sawo = {
-        "auth_key":"785ha-hdjsdsd-799-ss345",
-        "to":"login",
-        "identifier":"phone_number_sms"
+        "auth_key": API_KEY,
+        "to": "login",
+        "identifier": "email"
     }
-    return render_template("login.html",sawo=sawo,load=load)
+    return render_template("login.html", sawo=sawo, load=load)
 
-@app.route("/login",methods=["POST","GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
     payload = json.loads(request.data)["payload"]
     setLoaded(True)
     setPayload(payload)
     status = 200 if(verifyToken(payload)) else 404
-    return {"status" : status}
+    return {"status": status}
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     app.run()
